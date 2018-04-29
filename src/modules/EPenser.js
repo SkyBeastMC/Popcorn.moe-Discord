ffced3be5368b5556de406ac9e389c0f303880a4
SkyBeastMC <skybeastmc@gmail.com>
2018-04-29 21:10:56 +0200

    Lowercase rhinocéros check

--- src/modules/EPenser.js
+++ src/modules/EPenser.js
@@ -173,7 +173,7 @@ export default class EPenser {
 			readRules.announce && guild.channels.get(readRules.announceChannel);
 
 		if (
-			readRules.message === content ||
+			readRules.message === content.toLowerCase() ||
 			(typeof readRules.message === 'object' &&
 				readRules.message.includes(content))
 		)
