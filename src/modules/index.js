f9d48b310c96b994191e49768a35088402aaa4b4
SuwakoMmh <SuwakoMmh@users.noreply.github.com>
2019-03-01 05:21:03 +0100

    Delete meme.js

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,6 +3,5 @@ import EPenser from './EPenser';
 import Restart from './Restart';
 import Github from './Github';
 import Help from './Help';
-import Meme from './Meme';
 
 export { Licorne, EPenser, Github, Help, Restart, Meme };
