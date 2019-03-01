f1d83c81931cf48aa62b1d4cc2ad47dabc3d50e1
SuwakoMmh <SuwakoMmh@users.noreply.github.com>
2019-03-01 05:31:10 +0100

    Update index.js

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,6 +2,5 @@ import Licorne from './Licorne';
 import EPenser from './EPenser';
 import Restart from './Restart';
 import Github from './Github';
-import Help from './Help';
 
-export { Licorne, EPenser, Github, Help, Restart };
+export { Licorne, EPenser, Github, Restart };
