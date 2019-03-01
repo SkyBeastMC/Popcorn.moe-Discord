5005017ac17a5f4b1c6c53bc9bee2da9f4b6e7d8
SuwakoMmh <SuwakoMmh@users.noreply.github.com>
2019-03-01 05:27:33 +0100

    Remove meme

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,4 +4,4 @@ import Restart from './Restart';
 import Github from './Github';
 import Help from './Help';
 
-export { Licorne, EPenser, Github, Help, Restart, Meme };
+export { Licorne, EPenser, Github, Help, Restart };
